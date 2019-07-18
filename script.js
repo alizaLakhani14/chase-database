4f01f6f8575b58dd1427d3e27b84e944fadcccb1
alizaLakhani14 <49196299+alizaLakhani14@users.noreply.github.com>
2019-07-18 01:20:29 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+let chaseUp = {
+    name: "chaseUp",
+    presentInCities: ["karachi", "lahore", "Islamabad", "Peshawar"],
+    NoOfBranches: 15,
+    founder: "Mr.XYZ",
+    branch: {
+        city: "karachi",
+        location: "JailChowrangi",
+        SalesDepartment: {
+            SalesManager: "Mr.Saqib",
+            DeputySalesManager: "Mr.Hadi",
+            totalNoOfSalesPersons: 25,
+            totalNumberOfMaleSalesmen: 15,
+            totalNumberOfFemaleSaleswoman: 10,
+            NamesOfSalesMen: ["rafay", "Majid", "Sadiq", "Hassan", "Farhan", "Sohail", "Ahsan"],
+            NamesOfSalesWomen: ["Salma", "Sohaila", "Zamena", "Aliza", "Banin"]
+
+        },
+        FinanceDepartment: {
+            FinanceManager: "Mr.Mehboob",
+            DeputyFinanceManager: "Mr.Raza",
+            totalNoOfPeopleInFinanceDepartment: 25,
+            totalNumberOfMale: 15,
+            totalNumberOfFemale: 10,
+
+
+        },
+        totalNumberOfAisles: 15,
+        Amneties: {
+            Soaps: ["lux", "lifebouy", "dettol", "safeguard"],
+            shampoo: ["head&shoulders", "palmolive", "pantene"],
+            bodyWash: ["lux", "enchantuer", "lifebouy"],
+            handWash: ["lifebouy", "dettol", "hemaniHerbals"],
+            toothpaste:["colgate","closeUp","SodaWhite"]
+        },
+        Food:{
+            Pasta:["bakeparlour","kolson"],
+            rice:[
+                {
+                    name:"basmati",
+                    pricePerKg:100
+                },
+                {
+                    name:"noor",
+                    price:150
+                },
+                {
+                    name:"kajal",
+                    price:200
+                }
+            ],
+            flour:[
+                {
+                    name:"chakki",
+                    pricePerKg:25,
+                    type:"wholeWheat"
+                },
+                {
+                    name:"best-flour",
+                    pricePerKg:65,
+                    type:"All-purpose-flour"
+                },
+                {
+                    
+                }
+            ]
+        }
+
+
+
+    }
+}
